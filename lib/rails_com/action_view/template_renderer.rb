4c304676a4a68fff6a04dcfff007ce49b8395ad0
manxingxing <manxingxing@gmail.com>
2019-11-05 12:09:03 +0800

    don't mess with ActiveView's formats

--- lib/rails_com/action_view/template_renderer.rb
+++ lib/rails_com/action_view/template_renderer.rb
@@ -2,16 +2,10 @@
 
 module RailsCom
   module TemplateRenderer
-    
     def render(context, options)
-      _formats = [context.request.format.symbol].presence || @lookup_context.formats[0..0]
-      
-      # todo better implement
-      @lookup_context.send :_set_detail, :formats, _formats
       context.instance_variable_set(:@_rendered_template, options[:template])
       super
     end
-    
   end
 end
 
