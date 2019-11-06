477c21a83d7ceeff904554a56135c7940af2713b
manxingxing <manxingxing@gmail.com>
2019-11-06 12:15:02 +0800

    Revert "don't mess with ActiveView's formats"
    This reverts commit 4c304676a4a68fff6a04dcfff007ce49b8395ad0.

--- lib/rails_com/action_view/template_renderer.rb
+++ lib/rails_com/action_view/template_renderer.rb
@@ -2,8 +2,20 @@
 
 module RailsCom
   module TemplateRenderer
+
     def render(context, options)
+      if context.request
+        request_format = context.request.format.symbol
+        if request_format == :js
+          # workaround default 'html_fallback_for_js' behavior
+          # make js.erb View-Layer Inheritance possible
+          # https://github.com/rails/rails/issues/34499
+          @lookup_context.send :_set_detail, :formats, [:js]
+        end
+      end
+
       context.instance_variable_set(:@_rendered_template, options[:template])
+
       super
     end
   end
